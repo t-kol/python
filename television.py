60edb242f77c1f722db0eca095fc870a7a4f8efb
t-kol <timothy.kolster@gmail.com>
2026-04-14 08:39:18 -0500

    added docstring to init and made all instances private

--- television.py
+++ television.py
@@ -5,30 +5,33 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self) -> None:
-        self.status: bool = False
-        self.muted: bool = False
-        self.volume: int = Television.MIN_VOLUME
-        self.channel: int = Television.MIN_CHANNEL
+        """
+        Initializes variables
+        """
+        self.__status: bool = False
+        self.__muted: bool = False
+        self.__volume: int = Television.MIN_VOLUME
+        self.__channel: int = Television.MIN_CHANNEL
     
     def power(self) -> None:
         """
         Turns tv on and off by flipping boolean.
         """
-        self.status = not self.status
+        self.__status = not self.__status
 
     def mute(self) -> None:
         """
         If tv is on then either unmutes or mutes tv.
         If tv is muted and then is unmuted it will return to its previous volume (prev_volume).
         """
-        if self.status:
-            if not self.muted:
-                self.prev_volume = self.volume
-                self.volume = 0
-                self.muted = True
+        if self.__status:
+            if not self.__muted:
+                self.__prev_volume = self.__volume
+                self.__volume = 0
+                self.__muted = True
             else:
-                self.volume = self.prev_volume
-                self.muted = False
+                self.__volume = self.__prev_volume
+                self.__muted = False
 
     def channel_up(self) -> None:
         """
@@ -36,11 +39,11 @@ class Television:
 
         If channel is at the max channel then it circles around to the min channel.
         """
-        if self.status:
-            if self.channel == Television.MAX_CHANNEL:
-                self.channel = Television.MIN_CHANNEL
+        if self.__status:
+            if self.__channel == Television.MAX_CHANNEL:
+                self.__channel = Television.MIN_CHANNEL
             else:
-                self.channel += 1
+                self.__channel += 1
 
     def channel_down(self) -> None:
         """
@@ -48,11 +51,11 @@ class Television:
 
         If channel is at the min channel then it circles around to the max channel.
         """
-        if self.status:
-            if self.channel == Television.MIN_CHANNEL:
-                self.channel = Television.MAX_CHANNEL
+        if self.__status:
+            if self.__channel == Television.MIN_CHANNEL:
+                self.__channel = Television.MAX_CHANNEL
             else:
-                self.channel -= 1
+                self.__channel -= 1
 
     def volume_up(self) -> None:
         """
@@ -62,13 +65,13 @@ class Television:
 
         It does not increase if it is at max volume.
         """
-        if self.status:
-            if self.muted:
-                self.volume = self.prev_volume
-                self.muted = False
+        if self.__status:
+            if self.__muted:
+                self.__volume = self.__prev_volume
+                self.__muted = False
 
-            if self.volume < Television.MAX_VOLUME:
-                self.volume += 1
+            if self.__volume < Television.MAX_VOLUME:
+                self.__volume += 1
 
     def volume_down(self) -> None:
         """
@@ -78,16 +81,16 @@ class Television:
 
         It does not decreases if it is at min volume.
         """
-        if self.status:
-            if self.muted:
-                self.volume = self.prev_volume
-                self.muted = False
+        if self.__status:
+            if self.__muted:
+                self.__volume = self.__prev_volume
+                self.__muted = False
 
-            if self.volume > Television.MIN_VOLUME:
-                self.volume -= 1
+            if self.__volume > Television.MIN_VOLUME:
+                self.__volume -= 1
 
     def __str__(self) -> str:
         """
         Returns a string that includes the power status, channel number, and volume.
         """
-        return(f"Power = {self.status}, Channel = {self.channel}, Volume = {self.volume}")
+        return(f"Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}")
